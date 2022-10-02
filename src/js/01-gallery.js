3390310c55c03cd39ba032eced17ee1959c7b39d
AntonMaydan <92736982+AntonMaydan@users.noreply.github.com>
2022-10-02 20:52:35 +0300

    commit js 1
    commit js 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,33 @@
 // Add imports above this line
-import SimpleLightbox from "simplelightbox";
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
-console.log(SimpleLightbox);
 console.log(galleryItems);
+console.log(SimpleLightbox);
+
+const galleryEl = document.querySelector(".gallery");
+
+function makeGalleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+      <img class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+      />
+      </a>
+      </div>`;
+    })
+    .join("");
+}
+const list = makeGalleryItemsMarkup(galleryItems);
+galleryEl.insertAdjacentHTML("beforeend", list);
+
+new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+});
+
